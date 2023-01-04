324823b3529e1729d06e852d675759b803af1023
Gustav72 <wolfgangkirstein7@gmail.com>
2023-01-03 21:34:19 -0500

    Change board id's and add click events

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-const game = {
-    init: function() {
-        this.cacheDom();
-        this.bindEvents();
-        this.render();
-    },
-    cacheDOM: function() {
-    
-    },
-    bindEvents: function() {
+const gameBoard = function(){
+    let board = []
+    const init = function() {
+        cacheDOM();
+        bindEvents();
+    }
+    const cacheDOM = function() {
+        let container = document.getElementById('container');
+        for(let i = 0; i < 9; i++ ) {
+             board.push(document.getElementById(i));
+        }
+    }
+    const bindEvents = function() {
+        for(let i = 0; i < 9; i++ ) {
+            board[i].addEventListener("click", function() {
+                document.getElementById(i).innerHTML = 'x'
+            })
+        } 
+    }
+    return {
+        init
+    };
+}();
 
-    },
-    render: function() {
 
-    },
-}
 
+gameBoard.init();
